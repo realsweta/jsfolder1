ef87aa95c6848656f4211a1e79fd60eafeac2e79
bhartiSushma <enggsushma.21@gmail.com>
2025-07-27 13:19:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("delta 2'0")
+
+
+/*let marks = 95;
+
+if (marks >= 80) {
+    console.log("A+");
+} else if (marks >= 60) {
+    console.log("A");
+} else if (marks >= 33) {
+    console.log("B");
+} else if (marks <33) {
+    console.log("F");*/
